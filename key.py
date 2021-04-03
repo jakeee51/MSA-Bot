e13cda1f26992a4e710a9d5513e59b0f286074b2
David Jake Morfe <jakemorfe@gmail.com>
2021-04-02 23:23:11 -0400

    Initialize

---
+++ key.py
@@ -0,0 +1,43 @@
+import os
+
+CWD = os.getcwd(); ENV = "DEV"
+try:
+    os.chdir(".."); os.chdir(".."); os.chdir("..")
+    os.chdir("Desktop\\Prog\\InterMSA-Bot")
+except FileNotFoundError:
+    pass
+
+def bot_pass():
+   try:
+      with open("bot.txt") as f:
+         return f.read()
+   except FileNotFoundError:
+      pass
+
+def secret_pass():
+   try:
+      with open("sp.txt") as f:
+         return f.read()
+   except FileNotFoundError:
+      pass
+
+def db_pass():
+   try:
+      with open("db.txt") as f:
+         return f.read()
+   except FileNotFoundError:
+      pass
+
+def pub_pass():
+   try:
+      with open("pub.txt") as f:
+         return f.read()
+   except FileNotFoundError:
+      pass
+
+def email_pass():
+   try:
+      with open("email.txt") as f:
+         return f.read()
+   except FileNotFoundError:
+      pass
